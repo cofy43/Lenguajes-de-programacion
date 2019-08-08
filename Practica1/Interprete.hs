d6ed9f43d98ad9bdc20b0ad9641bab3ad46666fa
martin felipe <martin@debian.cofy43.com>
2019-08-08 17:19:50 -0500

    add complete

--- Practica1/Interprete.hs
+++ Practica1/Interprete.hs
@@ -1,6 +1,8 @@
 -- Declaracion de los tipos para el interprete
+
 data Instruction = N Int 
                  | Verdadero
+                 | Falso
                  | ADD
                  | DIV 
                  | EQ  
@@ -14,7 +16,18 @@ data Instruction = N Int
                  | SER
                  | SOB
                  | SOLAP
-
+                 deriving (Eq,Ord)
 -- Defición de los tipos para el interprete
 type Program = [Instruction]
 type Stack = [Instruction]
+
+instance Show Instruction where
+ show Verdadero = "V"
+ show Falso = "F"
+ show (N n) = show n
+
+addOperation :: Instruction -> Instruction -> Instruction
+addOperation (N n) (N m) = (N (n + m))
+addOperation (N n) x = error "Estimado usuario no es posible sumar un numero con una operacion"
+addOperation x (N n) = error "Estimado usuario no es posible sumar un numero con una operacion"
+addOperation x y = error "Estimado usuario no es posible sumar dos operaciones"
